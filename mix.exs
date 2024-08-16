1903409329e7f3281ec325e7403aabe6211915d9
Maxwell Muhanda <ajaybullec@gmail.com>
2024-08-16 22:24:54 +0300

    update mix

--- mix.exs
+++ mix.exs
@@ -1,15 +1,37 @@
 defmodule WhatsappElixir.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @repo_url "https://github.com/Maxino22/whatsapp_elixir"
+
   def project do
     [
       app: :whatsapp_elixir,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      description: "An Elixir library for WhatsApp API interactions",
-      deps: deps()
-    ]
+      deps: deps(),
+
+        # Hex
+      description: "Open source Elixir wrapper for the WhatsApp Cloud API",
+      package: package(),
+
+        # Docs
+      name: "whatsapp_elixir",
+      docs: [
+          name: "whatsapp_elixir",
+          source_ref: "v#{@version}",
+          source_url: @repo_url,
+          homepage_url: @repo_url,
+          main: "readme",
+          extras: ["README.md"],
+          links: %{
+            "GitHub" => @repo_url,
+            "Sponsor" => "https://github.com/sponsors/Maxino22"
+          }
+        ]
+      ]
+
   end
 
   def package do
