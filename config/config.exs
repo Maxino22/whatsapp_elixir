bf0a9691b8630cbcc815396b385047c141d464e7
Maxwell Muhanda <ajaybullec@gmail.com>
2024-08-21 10:50:42 +0300

    Revert "Made phone number id optional"

--- config/config.exs
+++ config/config.exs
@@ -1,16 +1,7 @@
 import Config
 
-config :whatsapp_elixir, WhatsappElixir.HTTP,
+
+config :whatsapp_elixir,
   token: "",
   phone_number_id: "",
-  verify_token: "",
-  base_url: "https://graph.facebook.com",
-  api_version: "v18.0"
-
-# Finally import the config/dev.local.exs which should NOT be versioned
-if File.exists?("#{Mix.env()}.local.exs") do
-  import_config "#{Mix.env()}.local.exs"
-else
-  require Logger
-  Logger.warning("Didn't find '#{Mix.env()}.local.exs'")
-end
+  verify_token: ""
